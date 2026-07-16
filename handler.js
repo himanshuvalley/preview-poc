3db278dcb0748c07a91d163bd7d80a7c5ec85a18
himanshu <himashu712@gmail.com>
2026-07-16 15:58:18 +0530

    test pr

--- handler.js
+++ handler.js
@@ -15,3 +15,10 @@ module.exports.hello = async (event) => {
 };
 // test Thursday 16 July 2026 03:50:55 PM IST
 // test Thursday 16 July 2026 03:57:11 PM IST
+// test                Local time: Thu 2026-07-16 15:58:03 IST
+           Universal time: Thu 2026-07-16 10:28:03 UTC
+                 RTC time: Thu 2026-07-16 10:28:03
+                Time zone: Asia/Kolkata (IST, +0530)
+System clock synchronized: yes
+              NTP service: active
+          RTC in local TZ: no
